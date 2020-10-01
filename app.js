7c93e50f873f7509e07af9b7ce8d249f06bcd8c4
sid <siddesht450@gmail.com>
2020-10-01 11:37:54 +0530

    ready to deploy

---
+++ app.js
@@ -0,0 +1,49 @@
+const path= require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postsRoute= require('./routes/posts');
+const userRoute= require('./routes/user');
+
+//connectiong to the database
+mongoose
+  .connect(
+    "mongodb+srv://Sid:jBWcaYOiVj9oZw9M@cluster0.xz4rb.mongodb.net/sid-post?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//creating app with express
+const app = express();
+
+//giving access to images folder
+app.use("/images", express.static(path.join("backend/images")));
+
+//parsing the incoming data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, useNewUrlParser: true }));
+
+//setting the CORS policy of headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH,OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postsRoute);
+app.use("/api/user", userRoute);
+
+
+module.exports = app;
---
+++ routes/posts.js
@@ -0,0 +1,142 @@
+const express= require('express')
+const multer = require('multer');
+
+const router= express.Router();
+
+const checkAuth =require("../middleware/check-auth");
+const MIME_TYPE_MAP= {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg'
+};
+
+const storage= multer.diskStorage({
+  destination: (req, file, cb)=>{
+  const isValid= MIME_TYPE_MAP[file.mimetype];
+  let error= new Error("invalid mime type");
+  if(isValid){
+    error= null;
+  }
+  cb(error, "backend/images");
+  },
+  filename: (req, file, cb)=>{
+    const name= file.originalname.toLowerCase().split(' ').join('-');
+    const ext =MIME_TYPE_MAP[file.mimetype];
+    cb(null, name +'-' +Date.now() + '.'+ext );
+  }
+})
+
+//importing our schema
+const Post = require("../models/post");
+
+
+//listing post requests
+router.post("",
+checkAuth, 
+multer({ storage: storage}).single("image"), 
+(req, res, next) => {
+  const url= req.protocol + "://" + req.get("host");
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: url + "/images/" +req.file.filename,
+      creator: req.userData.userId
+    });
+    post.save()
+    .then(data=>{
+      res.status(201).json({
+          message: "added successfully",
+          post: {
+          ...data,
+           id: data._id
+          }
+        });
+    });
+   
+  });
+  
+  
+  //listing the get Requests
+  router.get("", (req, res, next) => {
+  
+    const pageSize= +req.query.pagesize;
+    const currentPage= +req.query.page;
+    const postQuery= Post.find();
+    let fetchedPosts;
+    if(pageSize && currentPage){
+    postQuery
+    .skip(pageSize * (currentPage -1 ))
+    .limit(pageSize);
+    }
+
+    postQuery.find()
+      .then((documents) => {
+        fetchedPosts=documents;
+       return Post.countDocuments();
+      })
+      .then(count=>{
+        res.status(200).json({
+          message: "fetched successfully",
+          posts: fetchedPosts,
+          maxPosts: count
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+  
+  //getting a signle post
+  
+  router.get("/:id", (req, res)=>{
+    Post.findById(req.params.id).then(post=>{
+     if(post){
+     res.status(200).json(post);
+     }else{
+       res.status(404).json({message: 'post not found!'});
+     }
+    })
+  })
+  
+  //updating the posts
+  
+  router.put("/:id",checkAuth, multer({ storage: storage}).single("image"), (req, res)=>{
+    let imagePath = req.body.imagePath;
+    if(req.file){
+      const url= req.protocol + "://" + req.get("host");
+      imagePath = url + "/images/" +req.file.filename;
+    }
+    const post= new Post({
+      _id: req.body.id,
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: imagePath,
+      creator: req.userData.userId
+    });
+  
+  Post.updateOne({_id: req.params.id, creator: req.userData.userId}, post)
+  .then(result=>{
+    if(result.n >0){
+      res.status(200).json({message: "update successful!"});
+    }else{
+    res.status(401).json({message: "Not Authorized!"});
+    }
+  })
+  })
+  
+  
+  //listening the delete request
+  
+  router.delete("/:id",checkAuth, (req, res)=>{
+      Post.deleteOne({ _id: req.params.id, creator: req.userData.userId})
+      .then(result=>{
+        if(result.n >0){
+          res.status(200).json({message: "update successful!"});
+        }else{
+        res.status(401).json({message: "Not Authorized!"});
+        }
+      })
+  })
+  
+
+  module.exports= router;
